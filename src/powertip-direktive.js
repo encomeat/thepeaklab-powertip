621d77d3a0a2a49f4f957d11bc646ce711fb82a6
David Reiher <david@DR-iMac.local>
2016-03-15 16:33:27 +0100

    feat(color): added color-variable

--- src/powertip-direktive.js
+++ src/powertip-direktive.js
@@ -8,70 +8,111 @@
  * Supports placement of tooltips via `placement` Attribute.
  */
 
-angular.module('tpl.powertip', []).directive('powertip', function powertip() {
-  'use strict';
+angular.module('tpl.powertip', []).directive('powertip', [
+  '$compile',
+  '$document',
+  function powertip(
+    $compile,
+    $document) {
+    'use strict';
 
-  return {
-    template: '',
-    restrict: 'A',
-    scope: {
-      placement: '@powertipPlacement',
-      title: '=powertipTitle',
-      config: '='
-    },
-    link: function(scope, element, attrs) {
+    return {
+      template: '',
+      restrict: 'A',
+      scope: {
+        placement: '@powertipPlacement',
+        title: '=powertipTitle',
+        color: '=powertipColor',
+        config: '='
+      },
+      link: function(scope, element, attrs) {
 
-      // initially set title attribute on element
-      if (attrs.powertip) {
-        element[0].title = attrs.title;
+        var styleElems = [];
+
+        // initially set title attribute on element
+        if (attrs.powertip) {
+          element[0].title = attrs.title;
+        }
+
+        // build options
+        var options = {};
+
+        if (!scope.config) {
+          options.smartPlacement = true;
+
+          // config via attributes
+          if (scope.placement) {
+            options.placement = scope.placement;
+          }
+
+        } else {
+          // config via config-object
+          if (scope.config.placement) {
+            options.placement = scope.config.placement;
+          }
+
+          options.title = scope.config.title;
+        }
+
+        // init powertip
+        element.powerTip(options);
+
+        // add powertip class to tooltip-element to apply a generic tooltip style
+        element.on('powerTipRender', function() {
+          $('#powerTip').addClass('powertip');
+        });
+
+        scope.$watch('config', function(newVal) {
+          if (newVal && scope.config) {
+            element.data('powertip', scope.config.title);
+            // TODO: also update placement
+          }
+        });
+
+        scope.$watch('color', function(newVal) {
+          if (newVal) {
+
+            angular.forEach(styleElems, function(elem) {
+              elem.remove();
+            });
+
+            scope.color = newVal;
+
+            styleElems.push($compile('<style>' +
+              '.powertip' +
+              '{  border-bottom: 4px solid ' + newVal + ';}</style>')(scope));
+
+            styleElems.push($compile('<style>' +
+              '.powertip.n:before, .powertip.ne:before, .powertip.nw:before, ' +
+              '.powertip.ne-alt:before, .powertip.nw-alt:before' +
+              '{border-top: 10px solid' + newVal + ';}</style>')(scope));
+
+            styleElems.push($compile('<style>' +
+              '.powertip.s, .powertip.se, .powertip.sw, .powertip.se-alt, .powertip.sw-alt' +
+              '{border-top: 4px solid' + newVal + ';}</style>')(scope));
+
+            styleElems.push($compile('<style>' +
+              '.powertip.s:before, .powertip.se:before, .powertip.sw:before, ' +
+              '.powertip.se-alt:before, .powertip.sw-alt:before' +
+              '{border-bottom: 10px solid ' + newVal + ';}</style>')(scope));
+
+            angular.forEach(styleElems, function(elem) {
+              $document.find('head').append(elem);
+            });
+          }
+        });
+
+        // watch title for changes
+        scope.$watch('title', function(newVal) {
+          // powertip tooltip
+          element.data('powertip', newVal);
+        });
+
+        // cleanup
+        element.on('$destroy', function() {
+          element.powerTip('destroy');
+        });
       }
-
-      // build options
-      var options = {};
-
-      if (!scope.config) {
-        options.smartPlacement = true;
-
-        // config via attributes
-        if (scope.placement) {
-          options.placement = scope.placement;
-        }
-
-      } else {
-        // config via config-object
-        if (scope.config.placement) {
-          options.placement = scope.config.placement;
-        }
-
-        options.title = scope.config.title;
-      }
-
-
-      // init powertip
-      element.powerTip(options);
-
-      // add powertip class to tooltip-element to apply a generic tooltip style
-      element.on('powerTipRender', function() {
-        $('#powerTip').addClass('powertip');
-      });
-
-      scope.$watch('config', function(newVal) {
-        if (newVal && scope.config) {
-          element.data('powertip', scope.config.title);
-          // TODO: also update placement
-        }
-      });
-
-      // watch title for changes
-      scope.$watch('title', function(newVal) {
-        // powertip tooltip
-        element.data('powertip', newVal);
-      });
-
-      // cleanup
-      element.on('$destroy', function() {
-        element.powerTip('destroy');
-      });
-    }
-  };
-});
+    };
+  }
+]);
